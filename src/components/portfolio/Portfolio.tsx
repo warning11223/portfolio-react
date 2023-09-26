6f34ae567d7c743c5f5300acc0948dcc943d91d8
warning11223 <warning11223@gmail.com>
2023-09-26 21:59:19 +0300

    refactor: add new projects

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import pizza from '../../assets/pizza.png'
-import contacts from '../../assets/contacts.png'
-import ghSearch from '../../assets/gh-search.png'
-import airports from '../../assets/airports.png'
+
+import insta from '../../assets/portfolio/insta.png'
+import cards from '../../assets/portfolio/cards.png'
+import notes from '../../assets/portfolio/notes.png'
+import communicate from '../../assets/portfolio/communicate.png'
+import dashboard from '../../assets/portfolio/dashboard.png'
+import tesla from '../../assets/portfolio/tesla.png'
+import clinic from '../../assets/portfolio/clinic.png'
 
 import './Portfolio.css'
 
@@ -16,40 +20,68 @@ const Portfolio = () => {
                <div className="portfolio__items">
 
                    <article className='portfolio__item'>
-                       <img src={pizza} alt="image1" className='portfolio__img'/>
-                       <h3 className='portfolio__title'>Pizza Shop</h3>
+                       <img src={insta} alt="image" className='portfolio__img'/>
+                       <h3 className='portfolio__title'>Inctagram</h3>
                        <div className='portfolio__btns'>
-                           <a href="https://github.com/warning11223/react-pizza" className='portfolio__link btn ' target='_blank'>Github</a>
-                           <a href="https://warning11223.github.io/react-pizza/" className='portfolio__link btn btn-primary' target='_blank'>Take a look</a>
+                           <a href="https://github.com/LuVuitton/Inctagram" className='portfolio__link btn ' target='_blank'>Github</a>
+                           <a href="https://inctagram.vercel.app/" className='portfolio__link btn btn-primary' target='_blank'>Take a look</a>
                        </div>
                    </article>
 
                    <article className='portfolio__item'>
-                       <img src={contacts} alt="image1" className='portfolio__img'/>
-                       <h3 className='portfolio__title'>Contact List</h3>
+                       <img src={cards} alt="image" className='portfolio__img'/>
+                       <h3 className='portfolio__title'>Study with us</h3>
                        <div className='portfolio__btns'>
-                           <a href="https://github.com/warning11223/contacts-app" className='portfolio__link btn ' target='_blank'>Github</a>
-                           <a href="https://warning11223.github.io/contacts-app/" className='portfolio__link btn btn-primary' target='_blank'>Take a look</a>
+                           <a href="https://github.com/warning11223/study-with-us" className='portfolio__link btn ' target='_blank'>Github</a>
+                           <a href="https://warning11223.github.io/study-with-us/" className='portfolio__link btn btn-primary' target='_blank'>Take a look</a>
                        </div>
                    </article>
 
                    <article className='portfolio__item'>
-                       <img src={ghSearch} alt="image1" className='portfolio__img'/>
-                       <h3 className='portfolio__title'>Github search</h3>
+                       <img src={notes} alt="image" className='portfolio__img'/>
+                       <h3 className='portfolio__title'>Notes</h3>
                        <div className='portfolio__btns'>
-                           <a href="https://github.com/warning11223/github-search" className='portfolio__link btn ' target='_blank'>Github</a>
-                           <a href="https://warning11223.github.io/github-search/" className='portfolio__link btn btn-primary' target='_blank'>Take a look</a>
+                           <a href="https://github.com/warning11223/notes" className='portfolio__link btn ' target='_blank'>Github</a>
+                           <a href="https://warning11223.github.io/notes" className='portfolio__link btn btn-primary' target='_blank'>Take a look</a>
                        </div>
                    </article>
 
                    <article className='portfolio__item'>
-                       <img src={airports} alt="image1" className='portfolio__img'/>
-                       <h3 className='portfolio__title'>Airports app</h3>
+                       <img src={communicate} alt="image" className='portfolio__img'/>
+                       <h3 className='portfolio__title'>Communicate App</h3>
                        <div className='portfolio__btns'>
-                           <a href="https://github.com/warning11223/airports-app" className='portfolio__link btn ' target='_blank'>Github</a>
-                           <a href="https://warning11223.github.io/airports-app/" className='portfolio__link btn btn-primary' target='_blank'>Take a look</a>
+                           <a href="https://github.com/warning11223/communicate-app" className='portfolio__link btn ' target='_blank'>Github</a>
+                           <a href="https://warning11223.github.io/communicate-app/" className='portfolio__link btn btn-primary' target='_blank'>Take a look</a>
                        </div>
                    </article>
+
+                   <article className='portfolio__item'>
+                       <img src={dashboard} alt="image" className='portfolio__img'/>
+                       <h3 className='portfolio__title'>Dashboard</h3>
+                       <div className='portfolio__btns'>
+                           <a href="https://github.com/warning11223/react-dashboard" className='portfolio__link btn ' target='_blank'>Github</a>
+                           <a href="https://calm-dragon-9f6428.netlify.app/" className='portfolio__link btn btn-primary' target='_blank'>Take a look</a>
+                       </div>
+                   </article>
+
+                   <article className='portfolio__item'>
+                       <img src={tesla} alt="image" className='portfolio__img'/>
+                       <h3 className='portfolio__title'>Tesla</h3>
+                       <div className='portfolio__btns'>
+                           <a href="https://github.com/warning11223/tesla-lemplate" className='portfolio__link btn ' target='_blank'>Github</a>
+                           <a href="https://warning11223.github.io/tesla-lemplate/" className='portfolio__link btn btn-primary' target='_blank'>Take a look</a>
+                       </div>
+                   </article>
+
+                   <article className='portfolio__item'>
+                       <img src={clinic} alt="image" className='portfolio__img'/>
+                       <h3 className='portfolio__title'>Specialized clinic</h3>
+                       <div className='portfolio__btns'>
+                           <a href="https://github.com/warning11223/rocket-business" className='portfolio__link btn ' target='_blank'>Github</a>
+                           <a href="https://rocket-business-iota.vercel.app/" className='portfolio__link btn btn-primary' target='_blank'>Take a look</a>
+                       </div>
+                   </article>
+
 
                </div>
            </div>
